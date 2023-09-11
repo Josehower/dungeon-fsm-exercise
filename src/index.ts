013426bacd6ec5d5ac82ff76e8bee5c51814adf1
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2023-09-11 20:36:40 +0200

    Add boss damage states

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,34 @@
-import { assign, createMachine, interpret } from 'xstate';
+import { AnyEventObject, assign, createMachine, interpret } from 'xstate';
 
-type Context = { isBossAlive: boolean };
+type Context = { bossHealth: number };
 
-const killBoss = assign<Context>(() => {
+const hitBoss = assign<
+  Context,
+  { type: string; damage: number },
+  AnyEventObject
+>((context, event) => {
+  if (!event.damage) return context;
+
+  const newHealt = context.bossHealth - event.damage;
+
+  if (newHealt > 500) {
+    console.log("it doesn't seems to weak him that much");
+  } else if (newHealt <= 500 && newHealt > 0) {
+    console.log('It really hurts');
+  } else if (newHealt <= 0) {
+    console.log('Boss is dead!');
+  }
   return {
-    isBossAlive: false,
+    bossHealth: newHealt,
   };
 });
 
 function bossIsAlive(context: Context) {
-  return context.isBossAlive === true;
+  return context.bossHealth > 0;
 }
 
 function bossIsDead(context: Context) {
-  return context.isBossAlive !== true;
+  return context.bossHealth <= 0;
 }
 
 const dungeonMachine = createMachine(
@@ -22,7 +37,7 @@ const dungeonMachine = createMachine(
     initial: 'entrance',
     predictableActionArguments: true,
     context: {
-      isBossAlive: true,
+      bossHealth: 1000,
     } as Context,
     states: {
       entrance: { on: { ENTER: 'room_1' } },
@@ -37,9 +52,14 @@ const dungeonMachine = createMachine(
       room_3: { on: { SOUTH_DOOR: 'room_2', CROSS_PORTAL: 'win' } },
       fight: {
         on: {
-          WIN_FIGHT: { target: 'room_2', actions: 'killBoss' },
+          ATTACK_BOSS: { target: 'room_2', actions: 'hitBoss' },
           FLEE: 'room_1',
           DIE: 'dead',
+          LOG: { actions: 'prepareForFight' },
+        },
+        always: {
+          target: 'room_2',
+          cond: 'bossIsDead',
         },
       },
       dead: { type: 'final' },
@@ -47,19 +67,38 @@ const dungeonMachine = createMachine(
     },
   },
   {
-    actions: { killBoss },
+    actions: {
+      hitBoss: hitBoss,
+      prepareForFight: () =>
+        console.log(`The Boss is hungry and you are food.
+Prepare for fight`),
+    },
     guards: { bossIsDead, bossIsAlive },
   },
 );
 
 // Machine instance with internal state
-const toggleActor = interpret(dungeonMachine);
-toggleActor.subscribe((state) => console.log(state.value));
-toggleActor.start();
+const dungeonGame = interpret(dungeonMachine);
+dungeonGame.subscribe((state) => {
+  if (state.event.message) console.log(state.event.message);
+  if (state.event.type === 'ATTACK_BOSS') console.log(state.context);
+});
+
+dungeonGame.start();
 // => logs 'entrance'
 
-toggleActor.send({ type: 'ENTER' });
+dungeonGame.send({ type: 'ENTER', message: 'you are at the entrance' });
 // => logs 'room_1'
 
-toggleActor.send({ type: 'EAST_DOOR' });
+dungeonGame.send({ type: 'EAST_DOOR', message: 'A very dark room' });
 // => logs 'fight'
+dungeonGame.send({ type: 'LOG' });
+
+dungeonGame.send({ type: 'ATTACK_BOSS', damage: 300 });
+// => it doesn't seems to weak him that much
+dungeonGame.send({ type: 'ATTACK_BOSS', damage: 300 });
+// => It really hurts
+dungeonGame.send({ type: 'ATTACK_BOSS', damage: 300 });
+// => It really hurts
+dungeonGame.send({ type: 'ATTACK_BOSS', damage: 100 });
+// => Boss is dead!
